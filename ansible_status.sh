aef4600509ce96b0a658939d0f93125d0b31d7ef
Mohammed Aldaraji <71232730+theslash84@users.noreply.github.com>
2024-07-01 12:00:49 -0400

    Update ansible_status.sh

--- ansible_status.sh
+++ ansible_status.sh
@@ -1,53 +1,61 @@
 #!/bin/bash
 
-# Function to print ASCII art logo
-print_logo() {
-    echo "      _    _               _        "
-    echo "     / \  | |__   ___  ___| |_ __ _ "
-    echo "    / _ \ | '_ \ / _ \/ __| __/ _\` |"
-    echo "   / ___ \| | | |  __/ (__| || (_| |"
-    echo "  /_/   \_\_| |_|\___|\___|\__\__,_|"
-    echo
+# Function to get IP address of the machine
+get_ip() {
+  hostname -I | awk '{print $1}'
 }
 
-# Function to extract control machine details
-get_control_machine_details() {
-    echo "Control Machine:"
-    echo "Hostname: $(hostname)"
-    echo "IP Address: $(hostname -I | awk '{print $1}')"
-    echo "OS: $(lsb_release -ds)"
-    echo "Kernel: $(uname -r)"
-    echo
+# Function to get OS and Kernel info
+get_os_kernel() {
+  echo "$(lsb_release -d | awk -F"\t" '{print $2}'), Kernel: $(uname -r)"
 }
 
-# Function to extract managed nodes details
-get_managed_nodes_details() {
-    local inventory_file=$1
-    echo "Managed Nodes:"
-    grep -vE '^\s*$|^\s*#' "$inventory_file" | while IFS= read -r node; do
-        if [[ -n $node ]]; then
-            echo "Node: $node"
-            ansible -m ping -i "$inventory_file" "$node" > /dev/null 2>&1
-            if [[ $? -eq 0 ]]; then
-                echo "IP Address: $node"
-                echo "Status: SUCCESS"
-            else
-                echo "IP Address: $node"
-                echo "Status: FAILED"
-            fi
-            echo
-        fi
-    done
-}
-
-# Main script
-if [[ $# -ne 1 ]]; then
-    echo "Usage: $0 <inventory_file>"
-    exit 1
+# Check if inventory file is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <inventory_file>"
+  exit 1
 fi
 
-inventory_file=$1
+INVENTORY_FILE=$1
 
-print_logo
-get_control_machine_details
-get_managed_nodes_details "$inventory_file"
+# Control Machine Info
+CONTROL_HOSTNAME=$(hostname)
+CONTROL_IP=$(get_ip)
+CONTROL_OS_KERNEL=$(get_os_kernel)
+
+# Print Control Machine Info
+echo "  _____       _         ____                _        "
+echo " | ____| ____(_)  ___  |  _ \   ___ __   __(_)  ___  "
+echo " |  _|  |_  /| | / _ \ | | | | / _ \\\\ \ / /| | / _ \ "
+echo " | |___  / / | || (_) || |_| ||  __/ \ V / | || (_) |"
+echo " |_____|/___||_| \___/ |____/  \___|  \_/  |_| \___/ "
+echo
+echo "Control Machine:"
+echo "Hostname: $CONTROL_HOSTNAME"
+echo "IP Address: $CONTROL_IP"
+echo "OS: $CONTROL_OS_KERNEL"
+echo
+
+# Managed Nodes Info
+echo "Managed Nodes:"
+
+# Extract nodes from inventory file
+NODES=$(awk '/^\[/{flag=1; next} /^\]/{flag=0} flag' "$INVENTORY_FILE")
+
+for NODE in $NODES; do
+  NODE_NAME=$(echo $NODE | awk '{print $1}')
+  NODE_IP=$(echo $NODE | awk '{print $2}')
+  NODE_IP=${NODE_IP#*=}
+
+  echo "Node: $NODE_NAME"
+  echo "IP Address: $NODE_IP"
+
+  # Check node status using Ansible ping module
+  ansible -i "$INVENTORY_FILE" -m ping "$NODE_NAME" >/dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    echo "Status: SUCCESS"
+  else
+    echo "Status: UNREACHABLE"
+  fi
+  echo
+done
